dcf79f1a8cdb428f95925e70c964a0de5dbe9ee0
dagopia <tdaguti@gmail.com>
2023-05-10 07:53:24 -0700

    Search field componenet.

--- src/componenet/Search.js
+++ src/componenet/Search.js
@@ -1,36 +1,95 @@
 import * as React from "react";
+import { alpha, styled } from "@mui/material/styles";
+import InputBase from "@mui/material/InputBase";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import { Button } from "@mui/material";
 
+const CssTextField = styled(TextField)({
+  "& label.Mui-focused": {
+    color: "#A0AAB4",
+  },
+  "& .MuiInput-underline:after": {
+    borderBottomColor: "#B2BAC2",
+  },
+  "& .MuiOutlinedInput-root": {
+    "& fieldset": {
+      borderColor: "#E0E3E7",
+    },
+    "&:hover fieldset": {
+      borderColor: "#B2BAC2",
+    },
+    "&.Mui-focused fieldset": {
+      borderColor: "#6F7E8C",
+    },
+  },
+});
+const BootstrapInput = styled(InputBase)(({ theme }) => ({
+  "label + &": {
+    marginTop: theme.spacing(3),
+  },
+  "& .MuiInputBase-input": {
+    borderRadius: 4,
+    position: "relative",
+    backgroundColor: theme.palette.mode === "light" ? "#F3F6F9" : "#1A2027",
+    border: "1px solid",
+    borderColor: theme.palette.mode === "light" ? "#E0E3E7" : "#2D3843",
+    fontSize: 16,
+    width: "100%",
+    padding: "10px 12px",
+    transition: theme.transitions.create([
+      "border-color",
+      "background-color",
+      "box-shadow",
+    ]),
+    // Use the system font instead of the default Roboto font.
+    fontFamily: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
+    "&:focus": {
+      boxShadow: `${alpha(theme.palette.primary.main, 0.25)} 0 0 0 0.2rem`,
+      borderColor: theme.palette.primary.main,
+    },
+  },
+}));
 const SearchAccount = () => {
   return (
     <Box
       component='form'
       sx={{
-        width: "80%",
+        width: "100%",
         maxWidth: "100%",
         display: "flex",
         justifyContent: "center",
         alignContent: "center",
         alignItems: "center",
       }}
-      noValidate
       autoComplete='off'
     >
-      <TextField
+      <FormControl
+        variant='standard'
         sx={{
-          ml: 30,
-          my: 4,
+          //   ml: 30,
+          my: 5,
           bgcolor: "white",
           borderRadius: "5px",
         }}
-        id='search'
-        label='Search'
-        variant='outlined'
-        fullWidth
-      />
-      <Button variant='contained' sx={{ bgcolor: "#394867" }} size='large'>
+      >
+        <BootstrapInput id='bootstrap-input' placeholder='Search' />
+      </FormControl>
+
+      <Button variant='contained' sx={{ bgcolor: "#000000" }} size='large'>
         Go
       </Button>
     </Box>
