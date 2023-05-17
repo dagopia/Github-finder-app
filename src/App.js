b6b78cd779b1d6ccab7719a2fb6cdc8ae0664c6d
dagopia <tdaguti@gmail.com>
2023-05-17 05:26:35 -0700

    app comp rapped by Contect provider.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import logo from "./logo.svg";
 import { BrowserRouter, Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 
-// import MenuAppBar from "./componenet/AppBar";
-// import SearchAccount from "./componenet/Search";
-// import Footer from "./componenet/Footer";
-// import About from "./routes/About";
-// import Contact from "./routes/Contact";
+import { GithubProvider } from "./Context/github/GithubContext";
 import Home from "./routes/Home";
 import MenuAppBar from "./componenet/AppBar";
 import SearchAccount from "./componenet/Search";
@@ -16,39 +12,41 @@ import Footer from "./componenet/Footer";
 import NotFound from "./routes/NotFound";
 function App() {
   return (
-    <div className='App'>
-      <BrowserRouter>
-        <div>
-          <MenuAppBar />
+    <GithubProvider>
+      <div className='App'>
+        <BrowserRouter>
+          <div>
+            <MenuAppBar />
 
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route
-              path='/about'
-              element={
-                <About
-                  title=' Github Finder App'
-                  desc='This is Github Finder app for finding Github Users by Searching Them'
-                />
-              }
-            />
-            <Route
-              path='/contact'
-              element={
-                <Contact
-                  title=' Contact Me'
-                  desc='Contact us if you have any Quation!'
-                  Address='22 Golagole,Addis Ababa Ethiopia'
-                  phoNumber='+251999887766'
-                />
-              }
-            />
-            <Route path='/*' element={<NotFound />} />
-          </Routes>
-          <Footer />
-        </div>
-      </BrowserRouter>
-    </div>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route
+                path='/about'
+                element={
+                  <About
+                    title=' Github Finder App'
+                    desc='This is Github Finder app for finding Github Users by Searching Them'
+                  />
+                }
+              />
+              <Route
+                path='/contact'
+                element={
+                  <Contact
+                    title=' Contact Me'
+                    desc='Contact us if you have any Quation!'
+                    Address='22 Golagole,Addis Ababa Ethiopia'
+                    phoNumber='+251999887766'
+                  />
+                }
+              />
+              <Route path='/*' element={<NotFound />} />
+            </Routes>
+            <Footer />
+          </div>
+        </BrowserRouter>
+      </div>
+    </GithubProvider>
   );
 }
 
